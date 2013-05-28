1057c3ab876e4772b1259bfd36283d8e89051a1e
Shinnawat Viboonsitthichok <sviboonsitthichok@immap.org>
2013-05-28 20:37:32 +0700

    added Rest Service

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+use Immap\Database\PostgresSQL;
+require 'immap/database/PostgresSQL.php';
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/', function () {
+ 
+});
+
+$app->post('/formhub-json', function () use ($app) {
+          $request = $app->request();
+          $body = $request->getBody();
+          $json = json_decode($body);
+		  $pg_class = new PostgresSQL();
+	      $pg_class->db_connect();
+          $pg_class->save($json);
+		  $pg_class->db_close();  
+        });
+		
+$app->run();
+?>
